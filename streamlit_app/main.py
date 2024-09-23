1e07f5d26c056905dc0339d2948409034165cfcd
tobias <tobias.ettling@gmx.de>
2024-09-23 16:18:30 +0200

    LSTM Model

--- streamlit_app/main.py
+++ streamlit_app/main.py
@@ -2,6 +2,7 @@ from time import sleep
 
 import cv2
 import numpy as np
+import pandas as pd
 import streamlit as st
 from ultralytics import YOLO
 from PIL import Image
@@ -10,85 +11,99 @@ from src.pupil_capture import PupilCapture
 from src.model import ObjectDetector
 
 pupil_remote = PupilCapture()
-object_detector = ObjectDetector(model_path=".local/yolov8n.pt")
-
-# create a start button in streamlit
-start_button = st.button("Start Pupil Capture")
-stop_button = st.button("Stop Pupil Capture")
-
-# create a streamlit text container
-text_container = st.empty()
-
-# create a image container in streamlit
-image_container = st.empty()
 
 
+tab1, tab2 = st.tabs(["Object Detection", "Gaze Classifiaction"])
 
-if start_button:
+# Tab 1 content
+with tab1:
+    object_detector = ObjectDetector(model_path=".local/yolov8n.pt")
 
-    gaze_colors = [(55, 155, 255), (79, 55, 255), (170, 55, 255), (255, 55, 255), (0, 0, 255)]
-    while not stop_button:
-        sleep(0.01)
+    # create a start button in streamlit
+    start_button = st.button("Start Pupil Capture")
+    stop_button = st.button("Stop Pupil Capture")
 
-        # Receive the frame from the Pupil Capture
-        frame = pupil_remote.receive_frame_world_image()
-        gaze_points = []
-        for _ in range(3):
-            gaze = pupil_remote.receive_gaze_information()
-            gaze_points.append(gaze)
+    # create a streamlit text container
+    text_container = st.empty()
 
-        # remove point if distance two mean is grater than 2 times the standard deviation
-        gaze_points = np.array(gaze_points)
-        mean_gaze = np.mean(gaze_points, axis=0)
-        std_gaze = np.std(gaze_points, axis=0)
-        gaze_points = gaze_points[np.all(np.abs(gaze_points - mean_gaze) < 2 * std_gaze, axis=1)]
+    # create a image container in streamlit
+    image_container = st.empty()
 
 
 
-        # Predict the objects in the frame
-        curr_object = "Nothing"
-        results = object_detector.predict(frame)
-        for result in results:
-            boxes = result.boxes  # Boxes object for bounding box outputs
-            masks = result.masks  # Masks object for segmentation masks outputs
-            keypoints = result.keypoints  # Keypoints object for pose outputs
-            probs = result.probs  # Probs object for classification outputs
-            obb = result.obb
-            # print(f"Boxes: {boxes.xyxyn}")
-            # print the names of the objects detected
-            classes = boxes.cls
-            # print the names of the objects detected
-            for c in classes:
-                print(f"Names: {result.names[int(c)]}")
+    if start_button:
 
-            # if the mean gaze of the last 5 points is within the bounding box, print the name of the object
-            mean_gaze_point = np.mean(gaze_points, axis=0)
-            for i, box in enumerate(boxes.xyxyn):
-                if box[0] <= mean_gaze_point[0] <= box[2] and box[1] <= (1 - mean_gaze_point[1]) <= box[3]:
-                    curr_object = result.names[int(classes[i])]
+        gaze_colors = [(55, 155, 255), (79, 55, 255), (170, 55, 255), (255, 55, 255), (0, 0, 255)]
+        while not stop_button:
+            sleep(0.01)
 
-        # Display the current object
-        text_container.text(f"Current Object: {curr_object}")
+            # Receive the frame from the Pupil Capture
+            frame = pupil_remote.receive_frame_world_image(num_frames=1)[0]
+            gaze_points = pupil_remote.receive_gaze_position_information(num_gazes=5)
 
-        # Save the result image
-        object_detector.save_result(results)
+            # remove point if distance two mean is grater than 2 times the standard deviation
+            gaze_points = np.array(gaze_points)
+            mean_gaze = np.mean(gaze_points, axis=0)
+            std_gaze = np.std(gaze_points, axis=0)
+            gaze_points = gaze_points[np.all(np.abs(gaze_points - mean_gaze) < 2 * std_gaze, axis=1)]
+            mean_gaze = np.mean(gaze_points, axis=0)
 
-        # Display the result image
-        result_img_path = object_detector.image_path
-        result_img = Image.open(result_img_path)
 
-        result_img_array = cv2.cvtColor(np.array(result_img), cv2.COLOR_RGB2BGR)
-        for i, gaze_p in enumerate(gaze_points[-5:]):
-            if 0 <= gaze[0] <= 1 and 0 <= gaze[1] <= 1:
-                # Convert normalized gaze coordinates to pixel coordinates
-                # Flip the y-axis as the origin (0, 0) is in the top-left
-                # corner in OpenCV
-                gaze_x, gaze_y = int(gaze_p[0] * result_img_array.shape[1]), int((1 - gaze_p[1]) * result_img_array.shape[0])
 
-                # Draw a circle at the gaze point
-                cv2.circle(result_img_array, (gaze_x, gaze_y), 10, gaze_colors[-1*i], -1)
+            # Predict the objects in the frame
+            curr_object = "Nothing"
+            results = object_detector.predict(frame)
+            for result in results:
+                boxes = result.boxes  # Boxes object for bounding box outputs
+                masks = result.masks  # Masks object for segmentation masks outputs
+                keypoints = result.keypoints  # Keypoints object for pose outputs
+                probs = result.probs  # Probs object for classification outputs
+                obb = result.obb
+                # print(f"Boxes: {boxes.xyxyn}")
+                # print the names of the objects detected
+                classes = boxes.cls
+                # print the names of the objects detected
+                for c in classes:
+                    print(f"Names: {result.names[int(c)]}")
+                for i, box in enumerate(boxes.xyxyn):
+                    if box[0] <= mean_gaze[0] <= box[2] and box[1] <= (1 - mean_gaze[1]) <= box[3]:
+                        curr_object = result.names[int(classes[i])]
 
-        result_img = Image.fromarray(cv2.cvtColor(result_img_array, cv2.COLOR_BGR2RGB))
+            # Display the current object
+            text_container.text(f"Current Object: {curr_object}")
 
-        image_container.image(result_img)
+            # Save the result image
+            object_detector.save_result(results)
 
+            # Display the result image
+            result_img_path = object_detector.image_path
+            result_img = Image.open(result_img_path)
+
+            result_img_array = cv2.cvtColor(np.array(result_img), cv2.COLOR_RGB2BGR)
+            for i, gaze_p in enumerate(gaze_points[-5:]):
+                if 0 <= gaze_p[0] <= 1 and 0 <= gaze_p[1] <= 1:
+                    # Convert normalized gaze coordinates to pixel coordinates
+                    # Flip the y-axis as the origin (0, 0) is in the top-left
+                    # corner in OpenCV
+                    gaze_x, gaze_y = int(gaze_p[0] * result_img_array.shape[1]), int((1 - gaze_p[1]) * result_img_array.shape[0])
+
+                    # Draw a circle at the gaze point
+                    cv2.circle(result_img_array, (gaze_x, gaze_y), 10, gaze_colors[-1*i], -1)
+
+            result_img = Image.fromarray(cv2.cvtColor(result_img_array, cv2.COLOR_BGR2RGB))
+
+            image_container.image(result_img)
+
+# Tab 2 content
+with tab2:
+    st.write("Gaze Classification")
+    start_cls_button = st.button("Start Gaze Classification")
+
+    if start_cls_button:
+        gaze = pupil_remote.receive_gaze_information(num_gazes=240)
+        gaze_cols = list(gaze.columns)
+        gaze_values = gaze.values
+        # Downsampling from 120 to 30 Hz
+        gaze_values = gaze_values[::4]
+        gaze = pd.DataFrame(gaze_values, columns=gaze_cols)
+        st.write(gaze)
--- streamlit_app/src/pupil_capture.py
+++ streamlit_app/src/pupil_capture.py
@@ -1,3 +1,4 @@
+import pandas as pd
 import zmq
 import cv2
 import numpy as np
@@ -25,24 +26,68 @@ class PupilCapture:
 
 
 
-    def receive_frame_world_image(self):
+    def receive_frame_world_image(self, num_frames=1):
         sub_frame_world = self.ctx.socket(zmq.SUB)
         sub_frame_world.connect(f'tcp://{self.ip}:{self.sub_port}')
         sub_frame_world.subscribe("frame.world")
 
-        frame = sub_frame_world.recv_multipart()
-        image_bytes = frame[2]
-        decoded_frame = cv2.imdecode(np.frombuffer(image_bytes, dtype=np.uint8), cv2.IMREAD_COLOR)
-        rgb_frame = cv2.cvtColor(decoded_frame, cv2.COLOR_BGR2RGB)
-        img = Image.fromarray(rgb_frame)
-        return img
+        frames = []
+        for _ in range(num_frames):
+            frame = sub_frame_world.recv_multipart()
+            image_bytes = frame[2]
+            decoded_frame = cv2.imdecode(np.frombuffer(image_bytes, dtype=np.uint8), cv2.IMREAD_COLOR)
+            rgb_frame = cv2.cvtColor(decoded_frame, cv2.COLOR_BGR2RGB)
+            img = Image.fromarray(rgb_frame)
+            frames.append(img)
+        return frames
 
-    def receive_gaze_information(self):
+    def receive_gaze_position_information(self, num_gazes=1):
         subscriber = self.ctx.socket(zmq.SUB)
         subscriber.connect(f'tcp://{self.ip}:{self.sub_port}')
         subscriber.subscribe('gaze.')  # receive all gaze messages
+
+        gazes = []
+        for _ in range(num_gazes):
+            topic, payload = subscriber.recv_multipart()
+            message = msgpack.loads(payload)
+            gaze = message['norm_pos'.encode()]
+            gazes.append(gaze)
+        return gazes
+
+    def receive_gaze_information(self, num_gazes=1):
+        subscriber = self.ctx.socket(zmq.SUB)
+        subscriber.connect(f'tcp://{self.ip}:{self.sub_port}')
+        subscriber.subscribe('gaze.')
+
         topic, payload = subscriber.recv_multipart()
         message = msgpack.loads(payload)
-        gaze = message['norm_pos'.encode()]
 
-        return gaze
+        data = {
+            'gazeOrigin_x': message[b'eye_centers_3d'][b'0'][0],
+            'gazeOrigin_y': message[b'eye_centers_3d'][b'0'][1],
+            'gazeOrigin_z': message[b'eye_centers_3d'][b'0'][2],
+            'gazeDirection_x': message[b'gaze_normals_3d'][b'0'][0],
+            'gazeDirection_y': message[b'gaze_normals_3d'][b'0'][1],
+            'gazeDirection_z': message[b'gaze_normals_3d'][b'0'][2],
+            'gazePoint_x': message[b'gaze_point_3d'][0],
+            'gazePoint_y': message[b'gaze_point_3d'][1],
+            'gazePoint_z': message[b'gaze_point_3d'][2]
+        }
+        gaze_info = pd.DataFrame(data, index=[0])
+
+        for _ in range(num_gazes - 1):
+            topic, payload = subscriber.recv_multipart()
+            message = msgpack.loads(payload)
+            data = pd.DataFrame({
+                'gazeOrigin_x': message[b'eye_centers_3d'][b'0'][0],
+                'gazeOrigin_y': message[b'eye_centers_3d'][b'0'][1],
+                'gazeOrigin_z': message[b'eye_centers_3d'][b'0'][2],
+                'gazeDirection_x': message[b'gaze_normals_3d'][b'0'][0],
+                'gazeDirection_y': message[b'gaze_normals_3d'][b'0'][1],
+                'gazeDirection_z': message[b'gaze_normals_3d'][b'0'][2],
+                'gazePoint_x': message[b'gaze_point_3d'][0],
+                'gazePoint_y': message[b'gaze_point_3d'][1],
+                'gazePoint_z': message[b'gaze_point_3d'][2]
+            }, index=[0])
+            gaze_info = pd.concat([gaze_info, data], ignore_index=True)
+        return gaze_info
