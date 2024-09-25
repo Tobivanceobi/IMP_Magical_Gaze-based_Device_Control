124c72633f8c7f93759c36a89b3c59eb4f38969d
Tobivanceobi <57481909+Tobivanceobi@users.noreply.github.com>
2024-09-25 17:37:58 +0200

    sidebar_btn.py aktualisieren

--- Dashboard_App/src/components/sidebar_btn.py
+++ Dashboard_App/src/components/sidebar_btn.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-# Button component for uniform width in the sidebar
+
 def sidebar_button(label):
     key = "sidebar_btn_" + label
     return st.sidebar.button(label, key=key, use_container_width=True)
